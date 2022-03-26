6fc33da14fff2e33ba60bac767803623b357b14b
trgy2145 <trgy2145@gmail.com>
2022-03-26 10:22:09 +0100

    added_jsx-a11y

--- src/pages/details/Details.js
+++ src/pages/details/Details.js
@@ -15,61 +15,59 @@ const Details = (props) => {
     console.log(location.state.recipe)
     const  recipe=location.state.recipe
     return (
-       <DetailContainer>
-           <HeaderContainer>
-               <h1>{recipe.label}</h1>
-               <img src={dietSvg}/>
-           </HeaderContainer>
+      <DetailContainer>
+        <HeaderContainer>
+          <h1>{recipe.label}</h1>
+          <img src={dietSvg}/>
+        </HeaderContainer>
 
-            <DetailPart>
-            <OtherPart>
-          <>Nutrients</>
-          <p>
-            {recipe.totalNutrients.CA.label} :{' '}
-            {Math.round(recipe.totalNutrients.CA.quantity)}
-            {recipe.totalNutrients.CA.unit}
-          </p>
-          <p>
-            {recipe.totalNutrients.CHOCDF.label} :{' '}
-            {Math.round(recipe.totalNutrients.CHOCDF.quantity)}
-            {recipe.totalNutrients.CHOCDF.unit}
-          </p>
-          <p>
-            {recipe.totalNutrients.CHOLE.label} :{' '}
-            {Math.round(recipe.totalNutrients.CHOLE.quantity)}
-            {recipe.totalNutrients.CHOLE.unit}
-          </p>
-          <p>
-            {recipe.totalNutrients.ENERC_KCAL.label} :{' '}
-            {Math.round(recipe.totalNutrients.ENERC_KCAL.quantity)}
-            {recipe.totalNutrients.ENERC_KCAL.unit}
-          </p>
-          <p>{recipe.totalWeight}</p>
-          <p>Calories: {Math.round(recipe.calories)}</p>
-          {recipe.digest.slice(0, 4).map((item, index) => (
-            <p key={index}>
-              {item.label} : {Math.round(item.total)}
+        <DetailPart>
+          <OtherPart>
+            <>Nutrients</>
+            <p>
+              {recipe.totalNutrients.CA.label} :{" "}
+              {Math.round(recipe.totalNutrients.CA.quantity)}
+              {recipe.totalNutrients.CA.unit}
             </p>
-          ))}
-        </OtherPart>
-        <ImgContainer>
-          <img src={recipe.image} alt={recipe.label} />
-        </ImgContainer>
-        <IngContainer>
-          {recipe.ingredientLines.map((line, index) => (
-            <div key={index}>
-              <p>
-                {index + 1} - {line}
+            <p>
+              {recipe.totalNutrients.CHOCDF.label} :{" "}
+              {Math.round(recipe.totalNutrients.CHOCDF.quantity)}
+              {recipe.totalNutrients.CHOCDF.unit}
+            </p>
+            <p>
+              {recipe.totalNutrients.CHOLE.label} :{" "}
+              {Math.round(recipe.totalNutrients.CHOLE.quantity)}
+              {recipe.totalNutrients.CHOLE.unit}
+            </p>
+            <p>
+              {recipe.totalNutrients.ENERC_KCAL.label} :{" "}
+              {Math.round(recipe.totalNutrients.ENERC_KCAL.quantity)}
+              {recipe.totalNutrients.ENERC_KCAL.unit}
+            </p>
+            <p>{recipe.totalWeight}</p>
+            <p>Calories: {Math.round(recipe.calories)}</p>
+            {recipe.digest.slice(0, 4).map((item, index) => (
+              <p key={index}>
+                {item.label} : {Math.round(item.total)}
               </p>
-              <br />
-            </div>
-          ))}
-        </IngContainer>
-            </DetailPart>
-
-
-       </DetailContainer>
-    )
+            ))}
+          </OtherPart>
+          <ImgContainer>
+            <img src={recipe.image} alt={recipe.label} />
+          </ImgContainer>
+          <IngContainer>
+            {recipe.ingredientLines.map((line, index) => (
+              <div key={index}>
+                <p>
+                  {index + 1} - {line}
+                </p>
+                <br />
+              </div>
+            ))}
+          </IngContainer>
+        </DetailPart>
+      </DetailContainer>
+    );
 }
 
 export default Details
